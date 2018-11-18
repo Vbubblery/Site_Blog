b3888ddf67576877ba975655b4f2628456cae5c5
Juncheng ZHOU <157292@supinfo.com>
2018-11-18 12:59:16 +0800

    DB 1ST UPDATE

---
+++ models/markdownSchema.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const mdSchema = new mongoose.Schema({
+  title:{type:String,required: true},
+  author:{type:String,required: true},
+  body:{type:String,required: true},
+  comments:[{body:String,date:Date}],
+  meta:{
+    tags: { type: [String], index: true },
+    date: { type: Date, default: Date.now },
+  }
+});
+
+mdSchema.index({ date: 1, type: -1 });
+module.exports = mongoose.model('markdown',mdSchema);
---
+++ utils/dbConnections/markdownDB.js
@@ -0,0 +1,3 @@
+module.exports = {
+  database: process.env.MONGO_URI || 'mongodb://bubble:dx13658444998@ds039311.mlab.com:39311/markdowns'
+};
