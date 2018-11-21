2ab57a3312db6583510498f0420d123ed9c497b9
Juncheng ZHOU <157292@supinfo.com>
2018-11-21 12:33:50 +0800

    get title of markdown from db

--- components/MainContent.js
+++ components/MainContent.js
@@ -1,36 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import axios from 'axios';
 
 import mainContentStyle from '../styles/mainContentStyle';
 
-import Markdown from './Markdown';
-const htmlParser = require('react-markdown/plugins/html-parser')
 import MainSideBar from './MainSideBar'
 
 import { withStyles } from '@material-ui/core/styles';
 import {Grid,Typography,Divider,paper} from '@material-ui/core/';
 
-
-const post1 = "# This is a header\n\nAnd this is a paragraph\n\n<code>const htmlParser = require('react-markdown/plugins/html-parser')</code>";
-const post2 = '# 1This is a header\n\nAnd this is a paragraph';
-const post3 = '# 2This is a header\n\nAnd this is a paragraph';
-
-const posts = [post1,post2,post3];
-
-
-const parseHtml = htmlParser({
-  isValidNode: node => node.type !== 'script',
-  processingInstructions: [/* ... */]
-})
-
 class MainContent extends React.Component{
   constructor(props){
     super(props);
     this.classes = props.classes;
+    this.state = {mds:[]}
   }
-  
+  async componentDidMount(){
+    const res = await axios.get(`/api/allmd`);
+    this.setState((state, props) => ({
+      mds:res.data
+    }));
+    console.log(this.state)
+  }
   render(){
+    var posts = this.state.mds && this.state.mds.map(post=>{
+      return(
+        <Typography variant="h6" key={post._id}>
+          <Link href={`/post?id=${post._id}`}><a>{post.title}</a></Link>
+        </Typography>
+      )
+    });
+
     return(
       <React.Fragment>
         <Grid container spacing={40} className={this.classes.mainGrid}>
@@ -39,9 +40,7 @@ class MainContent extends React.Component{
                 Latest Blog Posts
             </Typography>
             <Divider />
-            {posts.map(post => (
-              <Markdown className={this.classes.markdown} key={post.substring(0, 40)} astPlugins={[parseHtml]} escapeHtml={false}>{post}</Markdown>
-              ))}
+            {posts}
           </Grid>
           <MainSideBar />
         </Grid>
